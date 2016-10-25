35f5eac8947e2d14ae4210a81f72133df8fee202
coolfy <3095059299@qq.com>
2016-10-25 21:47:07 +0800

    add-validate

--- DataBird-JavaWeb/src/main/webapp/js/validate.js
+++ DataBird-JavaWeb/src/main/webapp/js/validate.js
@@ -5,7 +5,7 @@
 
 /*校验登录*/
 $.validator.setDefaults({
-    submitHander:function () {
+    submitHandler:function () {
         alert("点击提交");
     }
 });
@@ -14,18 +14,26 @@ $().ready(function () {
     $("#loginForm").validate({
         rules:{
             username:{
-
+                required:true,
+                minlength:4,
+                maxlength:10
             },
             password:{
-
+                required:true,
+                minlength:6,
+                maxlength:16
             }
         },
         messages:{
             username:{
-
+                required:"请输入您的用户名",
+                minlength:"用户名至少由4个字母组成",
+                maxlength:"用户名最多由10个字母组成"
             },
             password:{
-
+                required:"请输入您的密码",
+                minlength:"密码至少由6位组成",
+                maxlength:"密码最多由16位组成"
             }
         }
     });
