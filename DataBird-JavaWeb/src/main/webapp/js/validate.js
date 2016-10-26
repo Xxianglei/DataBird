dbcdcba45a73624c890156cc58f57a21023450fb
coolfy <3095059299@qq.com>
2016-10-26 18:57:23 +0800

    add-pwdStyle

--- DataBird-JavaWeb/src/main/webapp/js/validate.js
+++ DataBird-JavaWeb/src/main/webapp/js/validate.js
@@ -27,8 +27,8 @@ $().ready(function () {
         messages:{
             username:{
                 required:"请输入您的用户名",
-                minlength:"用户名至少由4个字母组成",
-                maxlength:"用户名最多由10个字母组成"
+                minlength:"账号至少由4个字母组成",
+                maxlength:"账号最多由10个字母组成"
             },
             password:{
                 required:"请输入您的密码",
