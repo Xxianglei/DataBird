41b9493bef345b1adce003429b1b6a59aa854bcd
lcyanxi <845892601@qq.com>
2016-10-30 19:21:13 +0800

    updata-SimpleAction

--- DataBird-JavaWeb/src/main/java/cn/databird/action/SimpleAction.java
+++ DataBird-JavaWeb/src/main/java/cn/databird/action/SimpleAction.java
@@ -2,6 +2,7 @@ package cn.databird.action;
 
 import cn.databird.model.User;
 import cn.databird.service.SimpleService;
+import cn.databird.util.Base64Util;
 import com.opensymphony.xwork2.ActionSupport;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +14,7 @@ import java.util.Date;
 /**
  * Created by 言溪 on 2016/10/19.
  */
-@Component
+@Component("simpleAction")
 @Scope("prototype")
 public class SimpleAction extends ActionSupport {
     private static final Logger LOGGER=Logger.getLogger(SimpleAction.class);
@@ -22,10 +23,14 @@ public class SimpleAction extends ActionSupport {
 
     public String addUserTest(){
         User user=new User();
-        user.setAddress("河南周口");
         user.setLeaveTime(new Date());
         user.setStartTime(new Date());
-        user.setUserName("jieer");
+        user.setPassword("121542");
+        user.setEmail("845892601@qq.com");
+        user.setRealName("李常");
+        user.setUserName("lcyanxi");
+        user.setPassword(Base64Util.decodeBase64String("lc121718"));
+        user.setSex(true);
         simpleServie.SimpleTest(user);
         return SUCCESS;
 
