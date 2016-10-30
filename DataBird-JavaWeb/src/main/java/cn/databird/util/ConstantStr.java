0a98e0c7a965d20843a62d2650a2737df4bdc208
lcyanxi <845892601@qq.com>
2016-10-30 18:27:17 +0800

    add-util-ImageCodeUtil

--- DataBird-JavaWeb/src/main/java/cn/databird/util/ConstantStr.java
+++ DataBird-JavaWeb/src/main/java/cn/databird/util/ConstantStr.java
@@ -27,9 +27,29 @@ public class ConstantStr {
      */
     String str_one_hundred = "100";
     /**
+     * 字符串101
+     */
+    String str_one_hundred_one= "101";
+    /**
+     * 字符串102
+     */
+    String str_one_hundred_two = "102";
+    /**
+     * 字符串103
+     */
+    String str_one_hundred_three = "103";
+    /**
      * 字符串200
      */
     String str_two_hundred = "200";
+    /**
+     * 字符串201
+     */
+    String str_two_hundred_one = "201";
+    /**
+     * 字符串202
+     */
+    String str_two_hundred_two = "202";
     /**
      * 字符串300
      */
@@ -38,6 +58,10 @@ public class ConstantStr {
      * 字符串400
      */
     String str_four_hundred = "400";
+    /**
+     * 字符串404
+     */
+    String str_four_hundred_four = "404";
     /**
      * 字符串500
      */
@@ -171,9 +195,29 @@ public class ConstantStr {
      */
     int num_100 = 100;
     /**
+     * num 101
+     */
+    int num_101 = 101;
+    /**
+     * num 102
+     */
+    int num_102 = 102;
+    /**
+     * num 103
+     */
+    int num_103 = 103;
+    /**
      * num 200
      */
     int num_200 = 200;
+    /**
+     * num 201
+     */
+    int num_201 = 201;
+    /**
+     * num 202
+     */
+    int num_202 = 202;
     /**
      * num 300
      */
@@ -186,4 +230,12 @@ public class ConstantStr {
      * num 400
      */
     int num_400 = 400;
+    /**
+     * num 404
+     */
+    int num_404 = 404;
+    /**
+     * num 500
+     */
+    int num_500 = 500;
 }
